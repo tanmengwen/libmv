18ff4e91567a59c4c0158cb12576f4a4531e742e
pau.gargallo <pau.gargallo@1cef6b8b-5d4f-0410-83aa-f72ff1f650ef>
2008-06-17 17:11:50 +0000

    Frame computes sift features by call the external executable sift.

--- src/sparse/Frame.cc
+++ src/sparse/Frame.cc
@@ -1,19 +1,38 @@
 #include "Frame.h"
 
-
-void Frame::compute_sift_features()
-{
-	char buf[1000];
-	sprintf(buf, "sift %s --output=%s", image_filename.c_str(),sift_filename.c_str());
-
-	int res = system(buf);
+void Frame::set_filenames(const std::string &ifn) {
+	image_filename = ifn;
 }
 
 
-void Frame::load_sift_features()
+
+
+void Frame::compute_sift_features()
+{
+	int res;
+
+	const char *tmp_pgm = "/tmp/libmv_frame.pgm";
+	const char *tmp_key = "/tmp/libmv_frame.key";
+	char buf[1000];
+	sprintf(buf, "convert %s %s", image_filename.c_str(), tmp_pgm);
+	res = system(buf);
+
+	sprintf(buf, "sift %s --output=%s", tmp_pgm, tmp_key);
+	res = system(buf);
+
+	load_sift_features(tmp_key);
+
+	sprintf(buf, "rm %s", tmp_pgm);
+	res = system(buf);
+	sprintf(buf, "rm %s", tmp_key);
+	res = system(buf);
+}
+
+
+void Frame::load_sift_features(const char *sift_filename)
 {
 	FILE *fin;
-	fin = fopen(sift_filename.c_str(),"r");
+	fin = fopen(sift_filename,"r");
 
 	SiftFeature sf;
 	int res = fscanf(fin, "%f %f %f %f", &sf.x, &sf.y, &sf.scale, &sf.angle);
--- src/sparse/Frame.h
+++ src/sparse/Frame.h
@@ -12,8 +12,6 @@ class Frame
 {
 public:
 	std::string image_filename;
-	std::string sift_filename;  // sift features are computed by the
-				    // ../extern/siftpp/sift executable and stored in this file
 
 	int width, height;
 	std::vector<SiftFeature> features;
@@ -23,29 +21,10 @@ public:
 	Frame(const std::string &ifn) {
 		set_filenames(ifn);
 	}
-	void set_filenames(const std::string &ifn, const std::string &sfn="null") {
-		image_filename = ifn;
-
-		if( sfn == std::string("null") ) {
-			sift_filename = sift_filename_from_image_filename(ifn);
-		} else {
-			sift_filename = sfn;
-		}
-	}
-
-	static std::string sift_filename_from_image_filename( const std::string &ifn ) {
-		std::string res = ifn;
-
-		int dot=res.size()-1;
-		while(res[dot]!='.') dot--;
-
-		res.replace(dot,res.size()-dot,".key");
-
-		return res;
-	}
+	void set_filenames(const std::string &ifn);
 
 	void compute_sift_features();
-	void load_sift_features();
+	void load_sift_features(const char *sift_filename);
 };
 
 
