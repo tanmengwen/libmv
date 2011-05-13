901e4d93dea889632f2fd2b7eaac63a21183de46
julien.michot.fr <julien.michot.fr@1cef6b8b-5d4f-0410-83aa-f72ff1f650ef>
2011-05-13 16:23:09 +0000

    Removing obsolete branch from the repository

--- branches/libmv-eigen3/contrib/blender/create_patches.sh
+++
@@ -1,7 +0,0 @@
-#create the blender-libmv patch
-svn diff ../../../../source/blender/src/buttons_editing.c \
-         ../../../../source/blender/include/butspace.h \
-         ../../../../source/blender/makesdna/DNA_libmv_types.h \
-         ../../../../source/blender/makesdna/DNA_userdef_types.h \
-         ../../../../source/blender/makesdna/intern/makesdna.c \
-         > blender_libmv.diff
--- branches/libmv-eigen3/src/third_party/glog/src/config.h
+++
@@ -1,7 +0,0 @@
-#ifdef __APPLE__
- #include "config_mac.h"
-#elif __GNUC__
- #include "config_linux.h"
-#elif _MSC_VER
- #include "windows/config.h"
-#endif
